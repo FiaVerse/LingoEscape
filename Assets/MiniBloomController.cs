f5e17338727e09588c407dbfc78e201382b8d5f0
FiaVerse <114016094+FiaVerse@users.noreply.github.com>
2025-06-20 07:44:10 -0600

    Back Up

--- Assets/MiniBloomController.cs
+++ Assets/MiniBloomController.cs
@@ -3,12 +3,7 @@ using UnityEngine;
 public class MiniBloomController : MonoBehaviour
 {
     public GameObject radialMenu;
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-            radialMenu.SetActive(!radialMenu.activeSelf);
-    }
+    
     
     public void ToggleRadialMenu()
     {
